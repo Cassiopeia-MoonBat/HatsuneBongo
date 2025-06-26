cbf417da2cb852af4437365799b5791b6163cc84
Mina-Luna <boettchermina05@gmail.com>
2025-06-26 14:17:20 +0200

    hochwerf. guter name. ja. ts changes

--- code/code.js
+++ code/code.js
@@ -1,3 +1,13 @@
 "use strict";
-console.log("hello bitches");
+var Story;
+(function (Story) {
+    console.log(parent);
+    //console.log(parent.gameState);
+    if (parent.gameState == true) {
+        console.log("You can't get in again");
+        //     location.href = 
+    }
+    parent.gameState = true;
+    // console.log(parent.gamestate);
+})(Story || (Story = {}));
 //# sourceMappingURL=code.js.map
--- code/code.ts
+++ code/code.ts
@@ -1,1 +1,14 @@
-console.log("hello bitches");
+
+namespace Story{
+    type Parent = Window & { gameState: boolean};
+    declare let parent: Parent;
+
+    console.log(parent);
+    //console.log(parent.gameState);
+    if (parent.gameState == true) {
+        console.log("You can't get in again");
+        //     location.href = 
+    }
+    parent.gameState = true;
+    // console.log(parent.gamestate);
+}  
