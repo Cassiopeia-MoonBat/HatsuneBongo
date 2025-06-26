e213968ffb22fb352c06f62c33c916d471b790cd
Mina-Luna <boettchermina05@gmail.com>
2025-06-26 16:37:17 +0200

    alert and prompt inclusion

---
+++ code/codemath.js
@@ -0,0 +1,8 @@
+"use strict";
+alert("rechne du arsch!");
+let x = prompt("geb zahl ein, du nugatsüchtiger affe");
+let y = prompt("geb zweite zahl");
+let z;
+z = Number(x) * Number(y);
+alert(z);
+//# sourceMappingURL=codemath.js.map
---
+++ code/codemath.ts
@@ -0,0 +1,9 @@
+alert("rechne du arsch!");
+
+let x : number = prompt("geb zahl ein, du nugatsüchtiger affe");
+let y : number = prompt("geb zweite zahl");
+let z : number;
+
+z = Number(x) * Number(y);
+
+alert(z);
